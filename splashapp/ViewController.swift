e2893e5e62d1b555a09a2fc36aff125936cdff55
mescurra <>
2018-04-30 09:58:20 -0500

    version in splash

---
+++ splashapp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  splashapp
+//
+//  Created by mescurra on 3/20/18.
+//  Copyright © 2018 mescurra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imagen: UIImageView!
+    
+    @IBOutlet weak var open: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+ 
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
